d32228ea858bd1bfc12f1be1956f92de4d6835fb
Lindsey <lindseywoo@gmail.com>
2018-04-22 13:23:50 -0700

    added additional tests

--- dna/readAndWritePosts/readAndWritePosts.js
+++ dna/readAndWritePosts/readAndWritePosts.js
@@ -1,5 +1,6 @@
 function genesis() {
   commit("city", "Vancouver");
+  commit("city", "Kelowna");
   return true;
 }
 // this is called whenever a write/commmit
@@ -29,10 +30,13 @@ function validatePut(data) {
   return true;
 }
 
+/**
+ * creates a post linked to:
+ * - agent hash
+ * - city provided in the data
+ * - category provided in the data
+ **/
 function writePost(data) {
-  // returns a hash of the text param
-  // this hash how to access the text from
-  // the chain
   var hash = commit("postData", data);
   var me = App.Agent.Hash;
   commit("cityLinks", {
@@ -45,13 +49,44 @@ function writePost(data) {
       },
       {
         Base: makeHash("city", data["city"]),
+        Link: makeHash("category", data["category"]),
+        Tag: "postsByCityToCategory"
+      },
+      {
+        Base: makeHash("category", data["category"]),
         Link: hash,
-        Tag: "postsByCity"
+        Tag: "postsByCategory"
       }
     ]
   });
   return hash;
 }
+
+function readYourPosts() {
+  var agentHash = App.Agent.Hash;
+  debug(agentHash);
+  var allLinksForAgent = getLinks(agentHash, "postsByUser", {
+    Load: true
+  });
+  debug("Number of links: " + allLinksForAgent.length);
+  return allLinksForAgent.map(function(link) {
+    debug(JSON.stringify(link));
+    return link.Entry;
+  });
+}
+
+function readPostsByCity(city) {
+  var hashedCity = makeHash("city", city);
+  var linksForCity = getLinks(hashedCity, "postsByCity", { Load: true });
+
+  debug("Number of links: " + linksForCity.length);
+  return linksForCity.map(function(link) {
+    debug(JSON.stringify(link));
+    return link.Entry;
+  });
+}
+
+function readPostsByCityAndCategory(city, category) {}
 
 function readPost(hash) {
   // get returns entry corresponding to the hash
