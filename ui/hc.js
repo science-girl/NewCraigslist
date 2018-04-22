4c34dba257a9242782ac63373e0c66a9622e827c
Lindsey <lindseywoo@gmail.com>
2018-04-21 18:13:47 -0700

    added interface

--- ui/hc.js
+++ ui/hc.js
@@ -1,1 +1,11 @@
-function yourApp(){alert('your UI code here!')}
+function yourApp() {
+  alert("your UI code here!");
+}
+
+document.getElementById("createPostForm").onsubmit = function onSubmit(event) {
+  var title = document.getElementById("titlePostInput").value;
+  var details = document.getElementById("detailPostInput").value;
+  var category = document.getElementById("categoryPostInput");
+  category = category.options[category.selectedIndex].text;
+  var email = document.getElementById("emailInput").value;
+};
